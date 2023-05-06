11dba07d4b36333aa591aa1da6a7129f58b5b67f
bnguyen20 <81063168+bnguyen20@users.noreply.github.com>
2023-05-06 03:35:49 -0400

    Create radix_sort_parallel.h

---
+++ parallel_sorting/radix_sort_parallel.h
@@ -0,0 +1,45 @@
+/*
+File: radix_sort_parallel.h
+Author: Bryan Nguyen
+*/
+
+#ifndef COUNTING_SORT_PARALLEL
+#define COUNTING_SORT_PARALLEL
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <mpi.h>
+#include "../serial_sorting/radix_sort.h"
+#include "../data_structures/find_max.h"
+
+void RadixSortParallel(int *arr, int len, int p, int my_rank, MPI_Comm *comm, MPI_Status *status);
+
+/// @param arr The array to sort
+/// @param p The number of processes
+/// @param my_rank My MPI rank
+/// @param comm A MPI communicator
+/// @param status A MPI status pointer
+void RadixSortParallel(int *arr, int len, int p, int my_rank, MPI_Comm *comm, MPI_Status *status){
+    // find length of sub array to divide original array evenly based on number of threads
+    int count_array_size = (int)(ceil((double)(len) / (double)(p)));
+    int* newArr;
+    
+
+    RadixSort(arr1, arr2, length, max_val);
+    
+    if (TestSortCorrectness(newArr, length)) {
+        printf("The array sorted correctly.\n");
+    } else {
+        printf("The array did not sort correctly.\n");
+    }
+
+/*    MPI_Send(newArr, length, MPI_DOUBLE, 0, 1, *comm);*/
+    if(my_rank == 0){
+        /*for(int i = 0; i < p; i++){
+            MPI_Recv(tempArr, len, MPI_DOUBLE, i, 1, *comm, status);
+            print_array(tempArr, len);
+        }*/
+    }
+}
+
+#endif
