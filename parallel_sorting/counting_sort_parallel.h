f6475023160f73d4f820529e828fce2bd0498e81
bnguyen20 <81063168+bnguyen20@users.noreply.github.com>
2023-05-06 02:43:34 -0400

    Update counting_sort_parallel.h

--- parallel_sorting/counting_sort_parallel.h
+++ parallel_sorting/counting_sort_parallel.h
@@ -20,27 +20,8 @@ void CountingSortParallel(double *arr, int len, int p, int my_rank, MPI_Comm *co
 /// @param status A MPI status pointer
 void CountingSortParallel(double *arr, int len, int p, int my_rank, MPI_Comm *comm, MPI_Status *status) {
     
-    double* newArr;
 
-    newArr = (double*)malloc(len * sizeof(double));
-    int x = 0; 
-    int length = 0;
-    double tempArr[len];
-
-    double low = (1 / (double)p) * (double)my_rank;
-    double high = (1 / (double)p) * (double)(my_rank + 1);
-
-    printf("Thread:%d : %f, %f\n", my_rank, low, high);
-
-    for(int i = 0; i < len; i++){
-        if((arr[i] <= high) && (arr[i] > low)){
-            newArr[x] = arr[i];
-            x++; 
-            length++;
-        }
-    }
-
-    InsertionSort(newArr, length);
+    CountingSort(arr1, arr2, length, max_val);
     
     if (TestSortCorrectness(newArr, length)) {
         printf("The array sorted correctly.\n");
